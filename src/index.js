bffbecf4aa62450014bed1ea9e7f2c868920dbb2
juwel26 <rk370613@gmail.com>
2021-08-16 15:28:41 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM, { render } from 'react-dom';
+import './index.css'
 
+const img = 'https://picsum.photos/200/300';
+const img2 = 'https://picsum.photos/210/300';
+const img3 = 'https://picsum.photos/230/300';
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+  <h1 className='head'>Juwel rana</h1>
+  <div className='pic'>
+  <img src={img} alt='randomeimage' />
+  <img src={img2} alt='randomeimage' />
+  <img src={img3} alt='randomeimage' /> <br/>
+  <a href='http://developerjuwelrana.ezyro.com/' target='_blank'  rel="noreferrer">
+  <button>contact developer</button>
+  </a>
+  <br/>
+  <br/>  
+  <marquee className ='scrolloing'> hey! how are you? i hope you are well. so you 
+   need any awsome pic downlode frome here. and conract with me 
+   click on top button. thank you.</marquee>
+<p></p>
+</div> 
+  
+  <br/>
+  <div className='footer'>
+    <p>developer juwel rana.</p>
+  </div>
+   </>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
